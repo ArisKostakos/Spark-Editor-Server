aecb95a537b1684b4fc4c75ae7ae688401bca12a
Aris Kostakos <a.kostakos@gmail.com>
2015-02-06 20:29:48 -0300

    newcommit

--- game-server/app/servers/registration/handler/signupHandler.js
+++ game-server/app/servers/registration/handler/signupHandler.js
@@ -30,7 +30,7 @@ handler.signup = function(msg, session, next) {
 
 
      console.warn('The key I got was: ' + key);
-    console.warn('The crc32 of it is: ' + crc.crc32('hello').toString(16));
+    console.warn('The crc32 of it is: ' + crc.crc32('GT6UJG6E').toString(16));
 
 
     next(null, {
