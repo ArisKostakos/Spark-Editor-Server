5d7071c74701e1e7bce224d45241826345e1fc99
Aris Kostakos <a.kostakos@gmail.com>
2015-02-06 21:52:59 -0300

    newcommit

--- game-server/app/servers/registration/handler/signupHandler.js
+++ game-server/app/servers/registration/handler/signupHandler.js
@@ -30,9 +30,14 @@ handler.signup = function(msg, session, next) {
 
 
      console.warn('The key I got was: ' + key);
-    var kolos = crc.crc32('GT6UJG6E') >>>0;
 
-    console.warn('The crc32 of it is: ' + kolos.toString(16));
+    key = key.replace(/-/g, "");
+
+    console.warn('Removing dashes and its now: ' + key);
+
+    //var kolos = crc.crc32('GT6UJG6E') >>>0;
+
+    //console.warn('The crc32 of it is: ' + kolos.toString(16));
 
 
     next(null, {
