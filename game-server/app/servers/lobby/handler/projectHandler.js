f98158e50279a3843953793ae29b9e7d34f9b1a9
Aris Kostakos <a.kostakos@gmail.com>
2015-02-12 15:53:29 -0300

    newcommit

--- game-server/app/servers/lobby/handler/projectHandler.js
+++ game-server/app/servers/lobby/handler/projectHandler.js
@@ -23,23 +23,36 @@ var handler = Handler.prototype;
  */
 handler.create = function(msg, session, next) {
     var self = this;
-    /*
-    var uid = msg.uid;
-    var fullname = msg.fullname;
-    var email = msg.email;
-    var key = msg.key;
+
+    //var uid = msg.uid;
+    //var fullname = msg.fullname;
+    //var email = msg.email;
+    //var key = msg.key;
     var username = msg.username;
-    var password = msg.password;
-    */
+    //var password = msg.password;
+
     var sessionService = self.app.get('sessionService');
 
-    //create folders/files
-    //console.warn(fs.readdirSync(process.env['PWD']+"/../web-server/public"));
+    var publicPath = path.resolve("../web-server/public");
+    console.warn("resolve: " + publicPath);
 
-    console.warn("resolve 1: " + path.resolve("../web-server/public"));
+    var projectDir = crc.crc32(username) >>>0;
+    projectDir = pad(projectDir.toString(16),8);
+    projectDir = projectDir.toUpperCase();
 
+    var dir = publicPath + '/' + projectDir;
+    fs.ensureDir(dir, function(err) {
+        console.log(err); // => null
+        var l_code = err!=null?err:"projectCreated";
 
-    next(null, {
-        code: "Create Project complete"
-    });
+        next(null, {
+            code: l_code
+        });
+    })
 };
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
