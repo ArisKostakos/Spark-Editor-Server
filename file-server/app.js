04170ed55b1a9f27004a5bb728d54f61338e0db1
Aris Kostakos <a.kostakos@gmail.com>
2015-02-16 02:12:28 -0300

    newcommit

--- file-server/app.js
+++ file-server/app.js
@@ -7,10 +7,23 @@ var fs = require('fs-extra');
 var path = require('path');
 
 
+var publicPath = path.resolve("../web-server/public");
+var assetsPath = publicPath + '/assets';
+
 console.warn("File Server listening on: 3001");
 
 var server = BinaryServer({port: 3001});
 
 server.on('connection', function(client){
     console.warn("Somebody connected, yeahhh");
+    client.on('stream', function(stream, meta){
+        //
+        var file = fs.createWriteStream(assetsPath + '/' + meta.name);
+        stream.pipe(file);
+        //
+        // Send progress back
+        stream.on('data', function(data){
+            stream.write({rx: data.length / meta.size});
+        });
+    });
 });
