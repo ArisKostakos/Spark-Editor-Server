b882de316e6b2c8f909dc5db4cd0d77d3d2d6671
Aris Kostakos <a.kostakos@gmail.com>
2015-04-24 06:36:01 -0300

    commit

--- game-server/app/servers/assets/handler/uploadHandler.js
+++ game-server/app/servers/assets/handler/uploadHandler.js
@@ -360,7 +360,7 @@ handler.updateAssetFile = function(msg, session, next) {
                             //Asset Target Path
                             var assetTarget = assetPath + '/' + assetUserName + '/' + asset_found.type + '/' + asset_found.dir + '/' + asset_found.fileName + '.' + asset_found.fileExtension;
                             console.warn("Moving File: " + assetSource + ", to: " + assetTarget);
-
+                            //..
                             //Replace assetFile
                             fs.move(assetSource, assetTarget, {clobber:true}, function(err) {
                                 if (err) {
