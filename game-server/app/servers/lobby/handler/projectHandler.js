280175ef9247a0a4ade04c0a1bc8ab6b6366e918
Aris Kostakos <a.kostakos@gmail.com>
2015-09-30 10:14:26 +0300

    commit

--- game-server/app/servers/lobby/handler/projectHandler.js
+++ game-server/app/servers/lobby/handler/projectHandler.js
@@ -95,8 +95,7 @@ handler.fork = function(msg, session, next) {
                                         }
 
                                         //Handle Success
-                                        console.warn("save2");
-                                        module_found.save();
+
                                         //create new project (mark it forks ForkedProject, not a template, copy paste some ForkedProject stuff)
                                         var raw_Project = {name: projectName, version: '0.0.1', title: projectTitle, owner: developer._id, fork: templateProject._id, modules: [module_found._id], moduleMain: module_found._id, tags: [], includes: templateProject.includes, libraryCollections: templateProject.libraryCollections, accessControl: []};
 
