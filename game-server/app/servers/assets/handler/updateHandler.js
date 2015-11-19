342afa9145532a2a2cce23bcb6f09e55d78f9299
Aris Kostakos <a.kostakos@gmail.com>
2015-11-20 00:38:22 +0200

    commit

--- game-server/app/servers/assets/handler/updateHandler.js
+++ game-server/app/servers/assets/handler/updateHandler.js
@@ -75,3 +75,55 @@ handler.addProjectMainModuleAssetReferences = function(msg, session, next) {
         }
     );
 };
+
+handler.removeProjectMainModuleAssetReferences = function(msg, session, next) {
+    var self = this;
+    var sessionService = self.app.get('sessionService');
+
+    //Session bindings
+    var user = session.get('user');
+    var developer = session.get('developer');
+    var project = session.get('project');
+
+    var assetReferences = msg.assetReferences;
+
+    database.findOne(database.Module, {_id: project.moduleMain},
+        function (err, module_found) {
+            //Handle Error
+            if (err) {
+                next(null, {code: "error"});
+                return console.error(err);
+            }
+
+            console.warn("MAIN MODULE ASSETS: " + module_found.assets);
+            console.warn("ASSET REFERENCES: " + assetReferences);
+
+            //For each assetRef to remove
+            for (var i=0; i<assetReferences.length; i++)
+            {
+                // Find and remove item from an array
+                var indexFound = module_found.assets.indexOf(assetReferences[i]);
+                if(indexFound != -1) {
+                    module_found.assets.splice(indexFound, 1);
+                    console.warn("Removing Reference: " + assetReferences[i]);
+                }
+                else
+                {
+                    console.warn("(WARNING!) Asset does not exist. Doing Nothing: " + assetReferences[i]);
+                }
+            }
+
+            module_found.markModified('assets');
+            module_found.save(function (err) {
+                //Handle Error
+                if (err) {
+                    next(null, {code: "error"});
+                    return console.error(err);
+                }
+
+                //Handle Success
+                next(null, {code: "success"});
+            });
+        }
+    );
+};
