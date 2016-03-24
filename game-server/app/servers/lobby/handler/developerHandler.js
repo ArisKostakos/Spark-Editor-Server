f7a74b1370ce9684e0b931b71b6ad7dacefb04e0
Aris Kostakos <a.kostakos@gmail.com>
2016-03-24 22:29:55 +0200

    commit

---
+++ game-server/app/servers/lobby/handler/developerHandler.js
@@ -0,0 +1,36 @@
+var database = require('../../../modules/database');
+//var deepPopulate = require('mongoose-deep-populate');
+
+module.exports = function(app) {
+    return new Handler(app);
+};
+
+var Handler = function(app) {
+    this.app = app;
+};
+
+var handler = Handler.prototype;
+
+handler.populateDevelopers = function(msg, session, next) {
+    var self = this;
+
+    var developerIds = msg.developerIds;
+
+    console.warn("Requesting: " + developerIds);
+
+    //Find Developers
+    database.findAndPopulate(database.Developer, {owner: { $in: developerIds } }, "user team",
+        function (err, developers_found) {
+            //Handle Error
+            if (err) {
+                next(null, {code: "error"});
+                return console.error(err);
+            }
+
+            console.warn("Found: " + developers_found.length + " developers! yay!");
+
+            //Handle Success
+            next(null, {code: "success", developers: developers_found});
+        }
+    );
+};
