3eba2109b55c8c52c4f9948313627c5427f6105a
Aris Kostakos <a.kostakos@gmail.com>
2016-04-14 04:24:04 +0300

    commit

--- game-server/app/servers/multiplayer/handler/multiplayerHandler.js
+++ game-server/app/servers/multiplayer/handler/multiplayerHandler.js
@@ -11,42 +11,18 @@ var Handler = function(app) {
 
 var handler = Handler.prototype;
 
-/**
- * Gate handler that dispatch user to connectors.
- *
- * @param {Object} msg message from client
- * @param {Object} session
- * @param {Function} next next stemp callback
- *
- */
-handler.saySomething = function(msg, session, next) {
-	var uid = msg.uid;
-	if(!uid) {
-		next(null, {
-			code: 500
-		});
-		return;
-	}
-	// get all connectors
-	var connectors = this.app.getServersByType('connector');
-	if(!connectors || connectors.length === 0) {
-		next(null, {
-			code: 500
-		});
-		return;
-	}
-	// select connector
-	var res = dispatcher.dispatch(uid, connectors);
+handler.say = function(msg, session, next) {
+
+	var channel = this.channelService.getChannel("mainRoom", true);
+	var param = {
+		message: msg.message
+	};
+	channel.pushMessage('onSay', param);
+
 	next(null, {
-		code: 200,
-		host: res.host,
-		port: res.clientPort
+		code: "success"
 	});
 };
-
-//self.app.rpc.chat.chatRemote.add(session, uid, self.app.get('serverId'), "mainRoom", true, function(users){
-//ChatRemote.prototype.add = function(uid, sid, name, flag, cb) {
-
 
 
 handler.enter = function(msg, session, next) {
