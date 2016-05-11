222e7c72fe620716812c9f568a6c72e2ea038671
Aris Kostakos <a.kostakos@gmail.com>
2016-05-11 16:47:37 +0300

    commit

--- game-server/app/servers/multiplayer/handler/multiplayerHandler.js
+++ game-server/app/servers/multiplayer/handler/multiplayerHandler.js
@@ -28,6 +28,27 @@ handler.say = function(msg, session, next) {
 	};
 	channel.pushMessage('onSay', param);
 
+
+	//Get my Nickname (for later use on onLeave) (crap code I know)
+	if (session.get('username')==null)
+	{
+		var l_username = msg.id;
+		console.warn("setting username to: " + l_username);
+
+		//ok I have comfused myself here, but..
+		session.bind(l_username);
+		session.set('username', l_username);
+		session.push('username', function(err) {
+			if(err) {
+				console.error('set username for session service failed! error is : %j', err.stack);
+			}
+		});
+	}
+	else
+	{
+		console.warn("username not null it's: " + session.get('username'));
+	}
+
 	next(null, {
 		code: "success"
 	});
@@ -63,16 +84,6 @@ handler.enter = function(msg, session, next) {
 			console.error('set roomName for session service failed! error is : %j', err.stack);
 		}
 	});
-
-	//ok I have comfused myself here, but..
-	session.bind(l_username);
-	session.set('username', l_username);
-	session.push('username', function(err) {
-		if(err) {
-			console.error('set username for session service failed! error is : %j', err.stack);
-		}
-	});
-
 
 
 	var channel = this.channelService.getChannel(l_roomName, true);
@@ -110,8 +121,11 @@ var onUserLeave = function(app, session) {
 	var l_roomName = session.get('roomName');
 	var l_username = session.get('username');
 
+	if (l_username==null)
+		l_username="username Null";
+
 	console.warn('onLeave: my room name is: ' + l_roomName + ', my username is: ' + l_username);
-	var channel = this.channelService.getChannel(l_roomName, false);
+	var channel = app.get('channelService').getChannel(l_roomName, false);
 
 	var l_uid = session.uid;
 	var l_sid = app.get('serverId');
