cb1910f146b07821ac75fa1d0d46152a93d314a9
Aris Kostakos <a.kostakos@gmail.com>
2016-06-14 19:01:58 +0300

    commit

--- game-server/app/servers/assets/remote/createRemote.js
+++ game-server/app/servers/assets/remote/createRemote.js
@@ -58,7 +58,12 @@ remote.copy = function(asset, oldProjectName, user, developer, newProjectName, c
 				var newTags = asset.tags.slice(0); //clone
 				newTags[0]=newProjectName;
 
-				var raw_Asset = {name: asset.name.replace(oldProjectName,newProjectName), fork: asset._id, owner: developer._id, type: asset.type, dir: asset.dir.replace(oldProjectName,newProjectName), fileName: asset.fileName, fileExtension: asset.fileExtension, title: asset.title, fileSize: asset.fileSize, componentType: asset.componentType, tags: newTags, accessControl: [], assetDependancies: []};
+				//META
+				var newMeta = asset.meta;
+				console.warn("META:", newMeta);
+				newMeta.createdForProject = newProjectName;
+
+				var raw_Asset = {name: asset.name.replace(oldProjectName,newProjectName), fork: asset._id, owner: developer._id, type: asset.type, dir: asset.dir.replace(oldProjectName,newProjectName), fileName: asset.fileName, fileExtension: asset.fileExtension, title: asset.title, fileSize: asset.fileSize, componentType: asset.componentType, tags: newTags, meta: newMeta, accessControl: [], assetDependancies: []};
 
 				//Create Asset
 				database.create(database.Asset, raw_Asset,
